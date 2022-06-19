8705086bc32a77831bd98adb02cf6a75d7fe84c2
Daniel Rezende Lima <daniel-rl-98@hotmail.com>
2022-06-19 15:42:37 -0300

    first commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import LoginComponent from './pages/login/LoginComponent.vue'
+import HomeComponent from './pages/home/HomeComponent.vue'
 
 Vue.config.productionTip = false
 
+const routes = [
+  { path: '/', name: 'login', component: LoginComponent },
+  { path: '/home',name: 'home', component: HomeComponent}
+]
+
+const router = new VueRouter({
+  routes  
+})
+
+Vue.use(VueRouter)
+
+
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
---
+++ src/pages/login/LoginComponent.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="main">
+        <div class="login">
+            <div class="content-login">
+                <header class="content-title">
+                    <h1>Faça seu Login</h1>
+                </header>
+                <div class="form">
+                        <div class="field">
+                            <div class="content-label">
+                                <label for="Email">Email</label>
+                            </div>
+                            <p class="control has-icons-left has-icons-right">
+                                <input class="input is-primary" type="email" placeholder="Email">
+                                <span class="icon is-small is-left">
+                                    <i class="fas fa-envelope"></i>
+                                </span>
+                                <span class="icon is-small is-right">
+                                    <i class="fas fa-check"></i>
+                                </span>
+                            </p>
+                        </div>
+                         <div class="field">
+                             <div class="content-label">
+                                <label for="Password">Senha</label>
+                            </div>
+                            <p class="control has-icons-left">
+                                <input class="input is-primary" type="password" placeholder="Password">
+                                <span class="icon is-small is-left">
+                                <i class="fas fa-lock"></i>
+                                </span>
+                            </p>
+                        </div>
+                        <div class="field">
+                            <button class="button is-primary" @click.prevent="login">Entrar</button>
+                        </div>
+                    </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<!---javascript-->
+<script>
+export default {
+    name: "LoginComponent",
+    data(){
+        return{
+            
+        }
+    },
+    methods: {
+        login(){
+            this.$router.push({name: 'home'})
+        }
+    }
+}
+</script>
+
+<style lang="sass" src="./sass/_main.sass" scoped>
+
+</style>
